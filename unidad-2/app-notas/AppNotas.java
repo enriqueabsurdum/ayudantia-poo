13362248059cbc403dee5eccfa9ff72c861a7db2
enriqueabsurdum <enriqueabsurdum@gmail.com>
2017-10-20 19:50:58 -0300

    Update 20/10/2017

--- unidad-2/app-notas/AppNotas.java
+++ unidad-2/app-notas/AppNotas.java
@@ -4,78 +4,77 @@ import java.io.InputStreamReader;
 
 public class AppNotas {
 	
-	public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException {
 
     byte opcion = 0;
-		byte cant = 0;
+    byte cant = 0;
 		
-		float[] vectorNotas = new float[8];
-		Notas notas = new Notas();
+    float[] vectorNotas = new float[8];
+    Notas notas = new Notas();
 		
-		BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+    BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
 				
-		do {
-			System.out.println("\nMENÚ PRINCIPAL APLICACIÓN NOTAS\n"
-					+ "(1) Ingresar notas\n"
-					+ "(2) Listar notas\n"
-					+ "(3) Promediar notas\n"
-					+ "(4) Nota más alta\n" 
-					+ "(5) Nota más baja\n" 
-					+ "(6) Buscar nota específica\n" 
-					+ "(7) Modificar nota específica\n" 
-					+ "(8) Eliminar nota específica\n" 
-					+ "(9) Salir\n");
+    do {
+      System.out.println("\nMENÚ PRINCIPAL APLICACIÓN NOTAS\n"
+          + "(1) Ingresar notas\n"
+          + "(2) Listar notas\n"
+          + "(3) Promediar notas\n"
+          + "(4) Nota más alta\n" 
+          + "(5) Nota más baja\n" 
+          + "(6) Buscar nota específica\n" 
+          + "(7) Modificar nota específica\n" 
+          + "(8) Eliminar nota específica\n" 
+          + "(9) Salir\n");
 			
-			System.out.print("Seleccione una opción : ");
-			opcion = Byte.parseByte(entrada.readLine());
+      System.out.print("Seleccione una opción : ");
+      opcion = Byte.parseByte(entrada.readLine());
 			
-			switch (opcion) {
+      switch (opcion) {
 
-			case 1:
-				cant = notas.ingresarNotas(vectorNotas);
-				System.out.println("Se cargaron " + cant + " notas");
-				break;
+      case 1:
+        cant = notas.ingresarNotas(vectorNotas);
+        System.out.println("Se cargaron " + cant + " notas");
+        break;
 				
-			case 2:
-				notas.listarNotas(vectorNotas, cant);
-				break;
+      case 2:
+        notas.listarNotas(vectorNotas, cant);
+        break;
 					
-			case 3:
-				float promedio = notas.promediarNotas(vectorNotas, cant);
-				System.out.println("El promedio de las notas es : " + promedio);
-				break;
+      case 3:
+        float promedio = notas.promediarNotas(vectorNotas, cant);
+        System.out.println("El promedio de las notas es : " + promedio);
+        break;
 				
-			case 4:
-				float notaMayor = notas.notaMayor(vectorNotas, cant);
-				System.out.println("La nota mayor es : " + notaMayor);
-				break;
+      case 4:
+        float notaMayor = notas.notaMayor(vectorNotas, cant);
+        System.out.println("La nota mayor es : " + notaMayor);
+        break;
 			
-			case 5:
-				float notaMenor = notas.notaMenor(vectorNotas, cant);
-				System.out.println("La nota menor es : " + notaMenor);
-				break;
+      case 5:
+        float notaMenor = notas.notaMenor(vectorNotas, cant);
+        System.out.println("La nota menor es : " + notaMenor);
+        break;
 				
-			case 6:
+      case 6:
         notas.buscarNota(vectorNotas, cant);
-				break;
+        break;
 				
-			case 7:
+      case 7:
         notas.modificarNota(vectorNotas, cant);
-				break;
+        break;
 				
-			case 8:
-				notas.eliminarNota(vectorNotas, cant);
-				break;
+      case 8:
+        notas.eliminarNota(vectorNotas, cant);
+        break;
 				
-			case 9:
-				System.out.println("Fin de la aplicación");
-				break;
+      case 9:
+        System.out.println("Fin de la aplicación");
+        break;
 
-			default:
-				System.out.println("Opción no válida");
-				break;
-			}
-			
-		} while(opcion != 9);
-	}
+      default:
+        System.out.println("Opción no válida");
+        break;
+      }
+    } while(opcion != 9);
+  }
 }
--- unidad-2/app-notas/Notas.java
+++ unidad-2/app-notas/Notas.java
@@ -4,79 +4,80 @@ import java.io.InputStreamReader;
 
 public class Notas {
 
-	BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+  BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
 
 	// Método (1) Ingresar notas
-	public byte ingresarNotas(float[] vectorNotas) throws IOException {
+  public byte ingresarNotas(float[] vectorNotas) throws IOException {
 		
     float nota = 0;
 
-		System.out.print("Cantidad de notas : ");
-		byte cant = Byte.parseByte(entrada.readLine());
+    System.out.print("Cantidad de notas : ");
+    byte cant = Byte.parseByte(entrada.readLine());
 
-		for (byte i = 0; i < cant; i++) {
-			System.out.print("Ingresar nota " + (i + 1) + " : ");
-			nota = Float.parseFloat(entrada.readLine());
-			vectorNotas[i] = nota;
-		}
-		return (cant);
-	}
+    for (byte i = 0; i < cant; i++) {
+      System.out.print("Ingresar nota " + (i + 1) + " : ");
+      nota = Float.parseFloat(entrada.readLine());
+      vectorNotas[i] = nota;
+    }
+    return (cant);
+   }
 
-	// Método (2) Listar notas
-	public void listarNotas(float[] vectorNotas, byte cant) {
+  // Método (2) Listar notas
+  public void listarNotas(float[] vectorNotas, byte cant) {
 		
-		byte lista = 0;		
+    byte lista = 0;		
 
-		for (byte i = 0; i < cant; i++) {
-			lista++;
-			System.out.println("Nota en la posición número " + lista + " : " + vectorNotas[i]);
-		}
-	}
+    for (byte i = 0; i < cant; i++) {
+      lista++;
+      System.out.println("Nota en la posición número " + lista + " : " + vectorNotas[i]);
+    }
+   }
 
-	// Método (3) Promediar notas
-	public float promediarNotas(float[] vectorNotas, byte cant) {
+  // Método (3) Promediar notas
+  public float promediarNotas(float[] vectorNotas, byte cant) {
 		
-		float acum = 0;
+    float acum = 0;
 
-		for (byte i = 0; i < cant; i++) {
-			acum += vectorNotas[i];
-		}
-		float promedio = acum / cant;
-		return (promedio);
-	}
+    for (byte i = 0; i < cant; i++) {
+      acum += vectorNotas[i];
+    }
+    float promedio = acum / cant;
+    return (promedio);
+  }
 
-	// Método (4) Nota más alta
-	public float notaMayor(float[] vectorNotas, byte cant) {
+  // Método (4) Nota más alta
+  public float notaMayor(float[] vectorNotas, byte cant) {
 
-		float notaMayor = 0;
+    float notaMayor = 0;
 
-		notaMayor = vectorNotas[0];
+    notaMayor = vectorNotas[0];
 
-		for (byte i = 0; i < cant; i++) {
-			if (vectorNotas[i] > notaMayor) {
-				notaMayor = vectorNotas[i];
-			}
-		}
-		return (notaMayor);
-	}
+    for (byte i = 0; i < cant; i++) {
+    if (vectorNotas[i] > notaMayor) {
+      notaMayor = vectorNotas[i];
+      }
+    }
+    return (notaMayor);
+  }
 
-	// Método (5) Nota más baja
-	public float notaMenor(float[] vectorNotas, byte cant) {
+  // Método (5) Nota más baja
+  public float notaMenor(float[] vectorNotas, byte cant) {
 
     float notaMenor = 0;
 
-		notaMenor = vectorNotas[0];
+    notaMenor = vectorNotas[0];
 
-		for (byte i = 0; i < cant; i++) {
-			if (vectorNotas[i] < notaMenor) {
-				notaMenor = vectorNotas[i];
-			}
-		}
-		return (notaMenor);
-	}
+    for (byte i = 0; i < cant; i++) {
+      if (vectorNotas[i] < notaMenor) {
+        notaMenor = vectorNotas[i];
+      }
+    }
+    return (notaMenor);
+  }
 
-	// Método (6) Ver nota específica
-	public float buscarNota (float n[], int cant) throws IOException {
+  // Método (6) Ver nota específica
+  public float buscarNota (float n[], int cant) throws IOException {
+    
     byte buscar = 0, aux = 0;            
     System.out.print("Ingrese posición de la nota a buscar : ");
     buscar = Byte.parseByte(entrada.readLine());
@@ -86,10 +87,11 @@ public class Notas {
     System.out.println("Nota posición número " + aux + " : " + n[buscar]);
             
     return (buscar);
-    }   
+  }   
 
 	// Método (7) Modificar nota específica
   public float modificarNota(float n[], int cant) throws IOException {
+    
     byte modificar = 0, aux = 0;
         
     System.out.print("Ingrese el número de posición de la nota a modificar : ");
@@ -102,19 +104,19 @@ public class Notas {
     n[modificar] = Float.parseFloat(entrada.readLine());
         
     return (modificar);
-    }
+  }
 
-	// Método (6) Eliminar nota específica
-	public void eliminarNota(float[] vectorNotas, int cant) throws IOException {
+  // Método (6) Eliminar nota específica
+  public void eliminarNota(float[] vectorNotas, int cant) throws IOException {
 
-		System.out.print("Ingrese posición de la nota : ");
-		byte posicion = Byte.parseByte(entrada.readLine());
-		byte i = posicion;
-		posicion = (byte) (posicion - 1);
+    System.out.print("Ingrese posición de la nota : ");
+    byte posicion = Byte.parseByte(entrada.readLine());
+    byte i = posicion;
+    posicion = (byte) (posicion - 1);
 
-		System.out.println("Nota en la posición número " + i + " : " + vectorNotas[posicion]);
-		vectorNotas[posicion] = 0;
+    System.out.println("Nota en la posición número " + i + " : " + vectorNotas[posicion]);
+    vectorNotas[posicion] = 0;
 
-		System.out.println("Se elimino la nota de la posición número " + i);
-	}
+    System.out.println("Se elimino la nota de la posición número " + i);
+  }
 }
